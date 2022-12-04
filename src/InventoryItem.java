4f796bc43d953d62a3591f4cb64318e2074dcf21
ace7270 <ace7270@rit.edu>
2022-12-04 15:12:08 -0500

    added InventoryItem attributes and constructor, added some methods to Model

--- src/InventoryItem.java
+++ src/InventoryItem.java
@@ -1,2 +1,15 @@
 public class InventoryItem {
+    protected int itemID;
+    protected String itemName;
+    protected int currentStock;
+    protected double price;
+
+    public InventoryItem(int itemID, String itemName, int currentStock, double price) {
+        this.itemID = itemID;
+        this.itemName = itemName;
+        this.currentStock = currentStock;
+        this.price = price;
+    }
+
+
 }
--- src/Model.java
+++ src/Model.java
@@ -6,4 +6,25 @@ public class Model {
     public Model(ArrayList<InventoryItem> inventory) {
         this.inventory = inventory;
     }
+
+    public void addItem(InventoryItem item){
+        inventory.add(item);
+    }
+
+    public void removeItem(InventoryItem item){
+        inventory.remove(item);
+    }
+
+    public void removeItem(int itemID){
+        inventory.removeIf(item -> item.itemID == itemID);
+    }
+
+    public InventoryItem getItem(int itemID){
+        for (InventoryItem item: inventory){
+            if (item.itemID == itemID){
+                return item;
+            }
+        }
+        return null;
+    }
 }
