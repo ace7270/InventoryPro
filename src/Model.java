90dbca2c43ce61a880d5848fe63227c6ee3fec0d
ace7270 <ace7270@rit.edu>
2022-12-04 14:36:25 -0500

    created model class

---
+++ src/Model.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Model {
+    protected ArrayList<InventoryItem> inventory;
+
+    public Model(ArrayList<InventoryItem> inventory) {
+        this.inventory = inventory;
+    }
+}
